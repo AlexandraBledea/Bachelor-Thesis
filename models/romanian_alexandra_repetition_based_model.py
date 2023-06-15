0e3f6a92d251eb09cf46cd7df8a2d73afc6baf49
Bledea Mihaela Alexandra <bledea_alexandra@yahoo.com>
2023-06-15 03:11:03 +0300

    ??

--- models/romanian_alexandra_repetition_based_model.py
+++ models/romanian_alexandra_repetition_based_model.py
@@ -27,7 +27,7 @@ class RomanianAlexandraRepetitionBasedModel(Strategy, ABC):
         return enc
 
     def __load_standard_scaler(self):
-        scaler = joblib.load(self.__path + 'scaler.joblib')
+        scaler = joblib.load(self.__path + 'scaler.pkl')
         return scaler
 
     def execute(self, recording):
