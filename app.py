3938d96bb47e047e1507b063fee2319c52896006
Bledea Mihaela Alexandra <bledea_alexandra@yahoo.com>
2023-06-15 14:48:09 +0300

    added azure pem

--- app.py
+++ app.py
@@ -77,9 +77,11 @@ api.add_resource((RecordingsView), '/recordings', resource_class_kwargs={
 
 
 if __name__ == '__main__':
-    context.use_privatekey_file('speaksoul-privateKey.key')
-    context.use_certificate_file('speaksoul.crt')
-    app.run(debug=True)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain('path/to/azure_cert.pem')
+    # context.use_privatekey_file('speaksoul-privateKey.key')
+    # context.use_certificate_file('speaksoul.crt')
+    app.run(debug=True, ssl_context=context)
 
     # app.run(ssl_context='adhoc')
     # app.run(ssl_context='adhoc')
